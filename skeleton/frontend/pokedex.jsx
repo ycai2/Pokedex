66196965b4727dead2e6b1123382fee09e69d769
Yisheng Cai <cai.eason@gmail.com>
2016-09-29 10:53:00 -0400

    finish phase 1

---
+++ skeleton/frontend/pokedex.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<h1>pokedex?!?!</h1>, document.getElementById('root')); 
+});
